1061dabc0744c288d204d5d52e9ee68cbd49c5c9
Araav Nayak <104335810+AraavNayak@users.noreply.github.com>
2023-11-15 20:28:44 -0800

    Update ytAdSkipper.js

--- ytAdSkipper.js
+++ ytAdSkipper.js
@@ -2,5 +2,5 @@ var htmlVideoPlayer = document.getElementsByTagName('video')[0];
 let duration = htmlVideoPlayer.duration;
 htmlVideoPlayer.currentTime = duration;
 htmlVideoPlayer.play();
-var skipBtn = document.getElementsByClassName("ytp-ad-skip-button ytp-button");
+var skipBtn = document.getElementsByClassName("ytp-ad-skip-button-modern ytp-button");
 skipBtn[0].click();
